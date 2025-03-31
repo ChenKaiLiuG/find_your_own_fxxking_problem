cb9872cc9346ad9fede9ed897f92f7bb74cba0ce
ChenKaiLiuG <141424456+ChenKaiLiuG@users.noreply.github.com>
2025-04-01 00:35:54 +0800

    Create print.cpp

---
+++ print.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+int main() {
+    fs::path current_path = fs::
+    fs::path file_path = current_path / "找找自己問題.txt";
+
+    std::ifstream file(file_path);
+    if (file.is_open()) {
+        std::string line;
+        while (std::getline(file, line)) {
+            std::cout << line << std::endl;
+        }
+        file.close();
+    } else {
+        std::cerr << "無法開啟檔案: " << file_path << std::endl;
+    }
+
+    return 0;
+}
